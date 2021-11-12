f9511fd6c094680503f2fbc57d58172cf7bf602f
Alexandria <alxpettit@gmail.com>
2021-11-12 00:33:44 -0800

    cleaning up code a lil bit, and removing code from gitignore since it turns out template will end up with the file in commiit history anyway

--- set-project-name.py
+++ set-project-name.py
@@ -5,27 +5,32 @@
 import re, sys
 from pathlib import Path
 
-target = Path('./project.godot')
-template_name_line = re.compile("config/name=\"(.*)\"")
-text = open(target).read()
+def pexit(string: str, code: int):
+    print(string)
+    exit(code)
+
+target: Path = Path('./project.godot')
+template_name_line: re.Pattern = re.compile("config/name=\"(.*)\"")
+text: str = open(target).read()
 result = template_name_line.search(text)
 
+if result is None:
+    pexit(f'Incorrect file format in: {target}', 1)
+    
 start: str = text[:result.start(1)]
 end: str = text[result.end(1):]
 current_name: str = result.group(1)
-new_name: str = None
+new_name: str = ''
 
 print(f'Original project name: {current_name}')
 if len(sys.argv) > 1:
     new_name = sys.argv[1]
 else:
-    print(f'To change: {sys.argv[0]} [new project name]')
-    exit(1)
+    pexit(f'To change: {sys.argv[0]} [new project name]', 1)
 
 if current_name == new_name:
-    print(f'Names are identical. Change skipped.')
-    exit(0)
+    pexit(f'Names are identical. Change skipped.', 0)
 
 open(target, 'w').write(start + new_name + end)
 
-print(f'Project name has now been changed to {new_name}')
+pexit(f'Project name has now been changed to {new_name}', 0)
